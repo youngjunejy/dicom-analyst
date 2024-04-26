57901e564a9dc11844e70337cf5fa113f3394fee
Young June <youngjunejy@outlook.com>
2024-04-26 22:07:44 +0800

    chore:init repo

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
