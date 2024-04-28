ec160892eb5d70c9fd55d9d49b60029eb9721e57
Young June <youngjunejy@outlook.com>
2024-04-28 16:52:16 +0800

    chore: integrate with itk-wasm/dicom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,54 @@
-import { Button } from "antd";
-import Image from "next/image";
+"use client"
+
+import { InboxOutlined } from '@ant-design/icons';
+import Dragger from 'antd/es/upload/Dragger';
+import {
+  applyPresentationStateToImage,
+  readDicomEncapsulatedPdf,
+  structuredReportToHtml,
+  structuredReportToText,
+  readDicomTags,
+  readImageDicomFileSeries,
+  setPipelinesBaseUrl,
+  getPipelinesBaseUrl,
+} from "@itk-wasm/dicom"
+
+
+const props = {
+  name: 'file',
+  multiple: true,
+  showUploadList: {
+    showPreviewIcon: false,
+    showRemoveIcon: false,
+    showdownloadIcon: false,
+  },
+  beforeUpload: (file: any) => {
+    console.log('beforeUpload', file);
+
+    readDicomTags(file, {
+      webWorker: false
+    }).then((tags) => {
+      console.log('tags', tags);
+    })
+    // const reader = itk.
+    return false;
+  },
+
+};
 
 export default function Home() {
+
+
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-    </div>
+    <Dragger {...props}>
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">Click or drag file to this area to upload</p>
+      <p className="ant-upload-hint">
+        Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+        banned files.
+      </p>
+    </Dragger>
   );
 }
